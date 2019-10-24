6605f49c4527ae3807c9b3c11ead52b32ad3016d
Brad Taylor <bradtaylorsf@gmail.com>
2019-10-24 06:58:51 -0700

    added colors extension

--- src/components/extensions/Colors/v0.1.0/Colors.js
+++ src/components/extensions/Colors/v0.1.0/Colors.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Button } from '@contentful/forma-36-react-components';
-import ExtensionWrapper from '../../../shared/ExtensionWrapper';
+import {
+  has, isArray, each, get,
+} from 'lodash';
+import { init as initContentfulExtension } from 'contentful-ui-extensions-sdk';
+import './Colors.scss';
 
-function Colors() {
-  return (
-    <ExtensionWrapper>
-      <Button
-        buttonType="primary"
-        onClick={() => {
-          console.log('You clicked on Forma36 button');
-        }}
-      >
-        Click on me 0.1.0
+class Colors extends React.Component {
+  colorCardsArray = [];
 
-      </Button>
-    </ExtensionWrapper>
+  constructor(props) {
+    super(props);
+    this.state = {
+      api: {},
+      value: '',
+      hexs: [],
+    };
+  }
 
-  );
+  componentWillMount() {
+    initContentfulExtension((api) => {
+      this.setState({
+        value: api.field.getValue(),
+        api,
+        hexs: this.getHexOptions(api),
+      });
+      api.window.startAutoResizer();
+    });
+  }
+
+
+  getHexOptions = (api) => {
+    const { validations } = api.field;
+    const hexOptions = [];
+    each(validations, (val) => {
+      if (has(val, 'in') && isArray(get(val, 'in'))) {
+        each(get(val, 'in'), (hex) => {
+          hexOptions.push(hex);
+        });
+      }
+    });
+    return hexOptions;
+  }
+
+  handleColorChange = (e) => {
+    const hex = e.currentTarget.getAttribute('data-hex');
+    const { api } = this.state;
+    api.field.setValue(hex);
+    this.setState({
+      value: hex,
+    });
+  }
+
+  render() {
+    const { value, hexs } = this.state;
+    const dark = ['#000000', '#6B7C8D', '#445669'];
+    return (
+      <div id="colors">
+        {hexs.map((hex, index) => (
+          <div
+            className={`card ${dark.includes(hex) ? 'dark' : ''} ${value === hex ? 'selected' : ''}`}
+            style={{ backgroundColor: hex }}
+            data-hex={hex}
+            onClick={this.handleColorChange}
+            onKeyDown={this.handleColorChange}
+            role="button"
+            tabIndex={index}
+            key={hex}
+          >
+            <h4>
+              {hex}
+            </h4>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-Colors.propTypes = {
-
-};
 
 export default Colors;
