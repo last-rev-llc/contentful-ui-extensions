c3494ed044cf4293567cf2a982ac5c7cc463a92e
Brad Taylor <bradtaylorsf@gmail.com>
2019-10-25 12:56:17 -0700

    cleanup and jest setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,5 @@ module.exports = {
         implementation: require('sass'),
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
