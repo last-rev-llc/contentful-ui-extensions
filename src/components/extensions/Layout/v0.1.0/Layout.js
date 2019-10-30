457bb43fe379e785dad2eab22e6af9004af882d9
Brad Taylor <bradtaylorsf@gmail.com>
2019-10-30 09:58:25 -0700

    added layout

--- src/components/extensions/Layout/v0.1.0/Layout.js
+++ src/components/extensions/Layout/v0.1.0/Layout.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { init as initContentfulExtension } from 'contentful-ui-extensions-sdk';
+import { EntryCard, DropdownList, DropdownListItem } from '@contentful/forma-36-react-components';
 import GridLayout from 'react-grid-layout';
+import { get } from 'lodash';
 import '../../../../../node_modules/react-grid-layout/css/styles.css';
 import '../../../../../node_modules/react-resizable/css/styles.css';
+import resolveEntries from '../../../../global/scripts/resolveEntries';
 import './Layout.scss';
 
 function Layout() {
-  const [api, setApi] = useState({});
-  const [fieldConfig, setFieldConfig] = useState({});
+  const [fieldConfig, setFieldConfig] = useState(false);
+  const [api, setApi] = useState(false);
+  const [fullEntries, setFullEntries] = useState(false);
 
   useEffect(() => {
     initContentfulExtension((extensionApi) => {
-      console.log('wtf');
+      const getFullEntries = async () => {
+        setFullEntries(await resolveEntries(extensionApi.field.getValue(), extensionApi));
+      };
       setApi(extensionApi);
       setFieldConfig(extensionApi.entry.fields.layoutConfig.getValue());
+      getFullEntries();
       extensionApi.window.startAutoResizer();
     });
   }, []);
@@ -24,26 +31,49 @@ function Layout() {
       api.entry.fields.layoutConfig.setValue(layoutConfig);
     }
   };
-  console.log(api);
-  if (api && api.field) {
-    console.log('FIELD CONFIG', fieldConfig);
+
+  if (api && fieldConfig && fullEntries) {
     return (
       <>
+        {console.log('API', api)}
+        {console.log('FIELD CONFIG', fieldConfig)}
+        {console.log('FIELD ENTRIES', fullEntries)}
+
         <GridLayout
           className="layout"
           layout={fieldConfig}
           cols={12}
-          rowHeight={90}
+          rowHeight={150}
           width={700}
           onLayoutChange={handleLayoutChange}
         >
 
-          {api.field.getValue().map((content) => (
+          {fullEntries.map((content) => (
             <div
               key={content.sys.id}
             >
-              <div className="controls">...</div>
-              {content.sys.id}
+              <EntryCard
+                title={get(content, 'fields.title.en-US', 'Homepage Hero')}
+                description={get(content, 'fields.content.en-US', 'Some really cool description')}
+                status="published"
+                contentType={content.sys.contentType.sys.id}
+                dropdownListElements={(
+                  <>
+                    <DropdownList>
+                      <DropdownListItem isTitle>Actions</DropdownListItem>
+                      <DropdownListItem href="#">Edit</DropdownListItem>
+                      <DropdownListItem>
+                        Remove
+                      </DropdownListItem>
+                      <DropdownListItem isTitle>Align</DropdownListItem>
+                      <DropdownListItem>Right</DropdownListItem>
+                      <DropdownListItem>Left</DropdownListItem>
+                      <DropdownListItem>Center</DropdownListItem>
+
+                    </DropdownList>
+                  </>
+                )}
+              />
             </div>
           ))}
         </GridLayout>
