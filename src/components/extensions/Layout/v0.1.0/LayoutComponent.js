bacb8d33f0cf8ea70cc95762867fefdbbe01c3ee
Brad Taylor <bradtaylorsf@gmail.com>
2019-10-29 21:57:38 -0700

    testing layout

---
+++ src/components/extensions/Layout/v0.1.0/LayoutComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function LayoutComponent(props) {
+  return (
+    <div />
+  );
+}
+
+LayoutComponent.propTypes = {
+
+};
+
+export default LayoutComponent;
