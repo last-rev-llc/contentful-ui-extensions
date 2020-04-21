6ecc47c59523df0968cb6b046ef77982ef4a190b
ajwhitley <justin@lastrev.com>
2020-04-20 20:57:36 -0400

    RenderRow on FieldList

--- src/extensions/UiExtensions/components/FieldList.js
+++ src/extensions/UiExtensions/components/FieldList.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import FieldItem from './FieldItem';
 
 const FieldList = ({ sdk }) => {
-  const [fieldValue, setFieldValue] = useState({});
+  const [fieldValue, setFieldValue] = useState([]);
+
+  const renderRow = (id, key) => {
+    let objectValue = '';
+    if(key) {
+      objectValue = fieldValue[key];
+    }
+    return (
+      <div id="row">
+        <FieldItem 
+          sdk={sdk} 
+          key={key} 
+          objectValue={objectValue} />
+      </div>
+    );
+  };
 
   return (
-    <div id="row">
-      <FieldItem sdk={sdk} />
-    </div>
+    <>
+      {_.keys(fieldValue).map((key, i) => renderRow(i+1, key))}
+    </>
   );
 };
 
