1d0d8448501d58688dcdb5cd1effb3ea266c11ec
ajwhitley <justin@lastrev.com>
2020-05-01 00:07:59 -0400

    refactored; added cancel button

--- src/extensions/UiExtensions/components/FieldItem.js
+++ src/extensions/UiExtensions/components/FieldItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { Button } from '@contentful/forma-36-react-components';
--- src/extensions/UiExtensions/components/LocalizationLookup.js
+++ src/extensions/UiExtensions/components/LocalizationLookup.js
@@ -2,14 +2,33 @@ import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { TextInput, Button } from '@contentful/forma-36-react-components';
-// import FieldFactory from './FieldFactory';
-// import FieldList from './FieldList';
+// import FieldProperty from './FieldProperty';
 
 const buttonStyle = {
   marginTop: '-3px'
 };
 
-const NameField = (nameField, onNameChange, readOnly) => {
+const updateJson = (json, name, value) => {
+  return {
+    ...json,
+    [name]: value
+  };
+};
+
+const renderButton = (label, buttonType, onClick) => {
+  return <Button
+    style={buttonStyle}
+    buttonType={buttonType}
+    isFullWidth={false}
+    loading={false}
+    onClick={onClick}
+    testId="cf-ui-button"
+    type="button">
+    {label}
+  </Button>;
+};
+
+const renderNameField = (nameField, onNameChange, readOnly) => {
 
   return (
     <div>
@@ -27,7 +46,7 @@ const NameField = (nameField, onNameChange, readOnly) => {
   );
 };
 
-const ValueField = (valueField, onValueChange) => {
+const renderValueField = (valueField, onValueChange) => {
 
   return (
     <div>
@@ -44,186 +63,19 @@ const ValueField = (valueField, onValueChange) => {
   );
 };
 
-const FieldProperty = (nameField, valueField, onNameChange, onValueChange, readOnly) => {
+const renderFieldProperty = (nameField, valueField, onNameChange, onValueChange, readOnly) => {
   
   return (
     <div className="my-2">
       <div className="d-inline-block">
-        {NameField(nameField, onNameChange, readOnly)}
+        {renderNameField(nameField, onNameChange, readOnly)}
       </div>
       <span className="d-inline-block mx-3">:</span>
       <div className="d-inline-block">
-        {ValueField(valueField, onValueChange)}
+        {renderValueField(valueField, onValueChange)}
       </div>
     </div>
   );
-};
-
-const FieldFactory = (nameField, valueField, onNameChange, onValueChange, addProperty) => {
-
-  return (
-    <div>
-      <div className="d-inline-block">
-        {FieldProperty(nameField, valueField, onNameChange, onValueChange, false)}
-      </div>
-      <div className="d-inline-block ml-3">
-        <Button
-          style={buttonStyle}
-          buttonType="positive"
-          isFullWidth={false}
-          loading={false}
-          onClick={() => addProperty(nameField, valueField)}
-          testId="cf-ui-button"
-          type="button">
-          +
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-const FieldItem = (sdk, jsonObject, setJsonObject, nameField, valueField, editKey, setEditKey) => {
-  // const [name, setName] = useState(nameField);
-  // const [value, setValue] = useState(valueField);
-  // const [isDisabled, setIsDisabled] = useState(true);
-  // const [oldName, setOldName] = useState(nameField);
-  let name = nameField;
-  let value = valueField;
-  let oldName = nameField;
-  let disable = true;
-
-  if (editKey === name) {
-    disable = false;
-  }
-
-  const onDelete = () => {
-    const newObject = _.omit(jsonObject, [name]);
-    console.log('new object');
-    console.log(newObject);
-
-    sdk.field.setValue(newObject);
-    console.log('sdk');
-    console.log(sdk.field.getValue());
-
-    setJsonObject(newObject);
-    oldName = '';
-    console.log('json object');
-    console.log(jsonObject);
-  };
-
-  const onEditSave = () => {
-    const newObject = _.omit(jsonObject, [oldName]);
-    const newValue = {
-      ...newObject,
-      [_.camelCase(name)]: value
-    };
-    
-    sdk.field.setValue(newValue);
-    oldName = name;
-    setJsonObject(newValue);
-    console.log('in edit');
-    console.log(jsonObject);
-    setEditKey('');
-  };
-
-  const onNameChange = event => {
-    name = event.currentTarget.value;
-  };
-
-  const onValueChange = event => {
-    const newValue = {
-      ...jsonObject,
-      [name]: event.currentTarget.value
-    };
-    sdk.field.setValue(newValue);
-    setJsonObject(newValue);
-    value = event.currentTarget.value;
-  };
-  
-  return disable ? (
-    <div>
-      <div className="d-inline-block">
-        {FieldProperty(name, value, onNameChange, onValueChange, disable)}
-      </div>
-      <div className="d-inline-block ml-3">
-        <Button
-          style={buttonStyle}
-          buttonType="primary"
-          isFullWidth={false}
-          loading={false}
-          onClick={() => setEditKey(name)}
-          testId="cf-ui-button"
-          type="button">
-          Edit
-        </Button>
-      </div>
-      <div className="d-inline-block ml-1">
-        <Button
-          style={buttonStyle}
-          buttonType="negative"
-          isFullWidth={false}
-          loading={false}
-          onClick={onDelete}
-          testId="cf-ui-button"
-          type="button">
-          -
-        </Button>
-      </div>
-    </div>
-  ) : (
-    <div>
-      <div className="d-inline-block">
-        {FieldProperty(name, value, onNameChange, onValueChange, disable)}
-      </div>
-      <div className="d-inline-block ml-3">
-        <Button
-          style={buttonStyle}
-          buttonType="primary"
-          isFullWidth={false}
-          loading={false}
-          onClick={() => onEditSave()}
-          testId="cf-ui-button"
-          type="button">
-          Save
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-const FieldList = (sdk, jsonObject, setJsonObject, editKey, setEditKey) => {
-  console.log(sdk);
-  console.log(jsonObject);
-
-  const renderedList = _.keys(jsonObject).map((key, id) => {
-    let fieldValue = '';
-
-    if (key) {
-      fieldValue = jsonObject[key];
-    }
-    const divKey = id;
-
-    return (
-      <div 
-        id="row" 
-        key={divKey}>
-        {FieldItem(sdk, jsonObject, setJsonObject, key, fieldValue || '', editKey, setEditKey)}
-      </div>
-    );
-  });
-
-  return (
-    <>
-      {renderedList}
-    </>
-  );
-};
-
-const addProperty = (json, name, value) => {
-  return {
-    ...json,
-    [_.camelCase(name)]: value
-  };
 };
 
 const LocalizationLookup = ({ sdk }) => {
@@ -232,42 +84,159 @@ const LocalizationLookup = ({ sdk }) => {
   const [valueField, setValueField] = useState('');
   const [editKey, setEditKey] = useState('');
 
-  // sdk.field.setValue({});
-  // Sets the intial state value on component load to the Contentful value
   useEffect(() => {
     if(sdk.field.getValue()) {
       setJsonObject(sdk.field.getValue());
     }
   }, [sdk.field]);
 
-  // const setDisable = disable => {
-  //   setIsDisabled(disable);
-  //   return isDisabled;
-  // };
+  const onCancel = () => {
+    setEditKey('');
+  };
 
-  const onNameChange = event => {
+  const onNameFieldChange = event => {
     setNameField(event.currentTarget.value);
   };
 
-  const onValueChange = event => {
+  const onValueFieldChange = event => {
     setValueField(event.currentTarget.value);
   };
 
-  const addProp = () => {
-    const newJson = addProperty(jsonObject, nameField, valueField);
+  const addProperty = () => {
+    const newJson = updateJson(jsonObject, _.camelCase(nameField), valueField);
+
     sdk.field.setValue(newJson);
     setJsonObject(newJson);
     setNameField('');
     setValueField('');
   };
 
+  const renderFieldFactory = () => {
+
+    return (
+      <div>
+        <div className="d-inline-block">
+          {/* <FieldProperty 
+            nameField={nameField}
+            valueField={valueField}
+            onNameChange={onNameFieldChange} 
+            onValueChange={onValueFieldChange}
+            readOnly={false} /> */}
+          {renderFieldProperty(nameField, valueField, onNameFieldChange, onValueFieldChange, false)}
+        </div>
+        <div className="d-inline-block ml-3">
+          {renderButton('+', 'positive', () => addProperty(nameField, valueField))}
+        </div>
+      </div>
+    );
+  };
+
+  const renderFieldItem = (nameIn, valueIn) => {
+    let name = nameIn;
+    let value = valueIn;
+    let oldName = name;
+    const disable = !(editKey === name);
+  
+    const onDelete = () => {
+      const newObject = _.omit(jsonObject, [name]);
+  
+      sdk.field.setValue(newObject);
+      setJsonObject(newObject);
+      oldName = '';
+    };
+  
+    const onEditSave = () => {
+      const newObject = _.omit(jsonObject, [oldName]);
+      const newValue = updateJson(newObject, _.camelCase(name), value);
+      
+      sdk.field.setValue(newValue);
+      oldName = name;
+      setJsonObject(newValue);
+      setEditKey('');
+    };
+  
+    const onNameChange = event => {
+      name = event.currentTarget.value;
+    };
+  
+    const onValueChange = event => {
+      if (!editKey) {
+        const newValue = updateJson(jsonObject, name, event.currentTarget.value);
+  
+        sdk.field.setValue(newValue);
+        setJsonObject(newValue);
+      }
+      value = event.currentTarget.value;
+    };
+    
+    return disable ? (
+      <div>
+        <div className="d-inline-block">
+          {/* <FieldProperty 
+            nameField={name}
+            valueField={value}
+            onNameChange={onNameChange} 
+            onValueChange={onValueChange}
+            readOnly={disable} /> */}
+          {renderFieldProperty(name, value, onNameChange, onValueChange, disable)}
+        </div>
+        <div className="d-inline-block ml-3">
+          {renderButton('Edit', 'primary', () => setEditKey(name))}
+        </div>
+        <div className="d-inline-block ml-1">
+          {renderButton('-', 'negative', onDelete)}
+        </div>
+      </div>
+    ) : (
+      <div>
+        <div className="d-inline-block">
+          {/* <FieldProperty 
+            nameField={name}
+            valueField={value}
+            onNameChange={onNameChange} 
+            onValueChange={onValueChange}
+            readOnly={disable} /> */}
+          {renderFieldProperty(name, value, onNameChange, onValueChange, disable)}
+        </div>
+        <div className="d-inline-block ml-3">
+          {renderButton('Save', 'primary', onEditSave)}
+        </div>
+        <div className="d-inline-block ml-1">
+          {renderButton('Cancel', 'muted', onCancel)}
+        </div>
+      </div>
+    );
+  };
+
+  const renderFieldList = () => {
+
+    const renderedList = _.keys(jsonObject).map((key, id) => {
+      const divKey = id;
+      const fieldValue = key ? jsonObject[key] : '';
+  
+      return (
+        <div 
+          id="row" 
+          key={divKey}>
+          {renderFieldItem(key, fieldValue)}
+        </div>
+      );
+    });
+  
+    return (
+      <>
+        {renderedList}
+      </>
+    );
+  };
+
   return (
     <>
       <div>
-        {FieldFactory(nameField, valueField, onNameChange, onValueChange, addProp)}
+        {renderFieldFactory()}
       </div>
       <div className="mt-4">
-        {FieldList(sdk, jsonObject, setJsonObject, editKey, setEditKey)}
+        {renderFieldList()}
       </div>
     </>
   );
