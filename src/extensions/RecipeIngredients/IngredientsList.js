d9416e47c7f267b457f6fada1382d9073f1fd64b
Anthony Whitley <justin@lastrev.com>
2020-07-31 12:24:16 -0400

    Made ingredients object into array of objects

--- src/extensions/RecipeIngredients/IngredientsList.js
+++ src/extensions/RecipeIngredients/IngredientsList.js
@@ -7,20 +7,24 @@ import { openDialog } from './dialogs';
 
 
 const IngredientsList = ({ sdk }) => {
-  const [ingredientList, setIngredientList] = useState({ ingredients: [] });
+  const [ingredientList, setIngredientList] = useState([]);
 
   useEffect(() => {
     if(sdk.field.getValue()) {
-      setIngredientList(sdk.field.getValue());
+      let fieldValue = sdk.field.getValue();
+      if (!Array.isArray(fieldValue)) {
+        fieldValue = fieldValue.ingredients || [];
+      }
+      setIngredientList(fieldValue);
     }
   }, [sdk.field]);
 
   const addIngredient = (ingredient) => {
     if (ingredient) {
-      const ingredients = [ ...ingredientList.ingredients ];
+      const ingredients = [ ...ingredientList ];
       ingredients.push(ingredient);
-      sdk.field.setValue({ ingredients });
-      setIngredientList({ ingredients });
+      sdk.field.setValue(ingredients);
+      setIngredientList(ingredients);
     }
   };
 
@@ -30,30 +34,30 @@ const IngredientsList = ({ sdk }) => {
   };
 
   const editIngredient = (ingredient, ingredientIndex) => {
-    const ingredients = [ ...ingredientList.ingredients ];
+    const ingredients = [ ...ingredientList ];
     if (ingredient && ingredients[ingredientIndex]) {
       ingredients[ingredientIndex] = ingredient;
-      sdk.field.setValue({ ingredients });
-      setIngredientList({ ingredients });
+      sdk.field.setValue(ingredients);
+      setIngredientList(ingredients);
     }
   };
 
   const openEditModal = async (ingredientIndex) => {
-    const result = await openDialog(sdk, 'Edit Ingredient', { ingredient: ingredientList.ingredients[ingredientIndex] });
+    const result = await openDialog(sdk, 'Edit Ingredient', { ingredient: ingredientList[ingredientIndex] });
     editIngredient(result, ingredientIndex);
   };
 
   const deleteStep = (ingredientIndex) => {
     if (ingredientIndex > -1) {
-      const ingredients = ingredientList.ingredients.filter((ingredient, i) => i !== ingredientIndex);
-      sdk.field.setValue({ ingredients });
-      setIngredientList({ ingredients });
+      const ingredients = ingredientList.filter((ingredient, i) => i !== ingredientIndex);
+      sdk.field.setValue(ingredients);
+      setIngredientList(ingredients);
     }
   };
 
   return (
     <>
-      {getIngredientsTable(ingredientList.ingredients, openEditModal, deleteStep)}
+      {getIngredientsTable(ingredientList, openEditModal, deleteStep)}
       <div id='add-table-row-wrap'>
         {getIconButton('Click to add a new row', 'positive', 'PlusCircle', 'large', openAddModal)}
       </div>
