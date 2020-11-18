808e51021ff3b5db69fbde4da878bde712be8df1
Will Whitty <tavurth@gmail.com>
2020-11-18 13:33:11 +0300

    Work on add content

--- src/extensions/TemplateModules/TemplateModules.js
+++ src/extensions/TemplateModules/TemplateModules.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { EntryCard, CardDragHandle, DropdownList, DropdownListItem } from '@contentful/forma-36-react-components';
+import {
+  CardDragHandle,
+  DropdownList,
+  DropdownListItem,
+  EntryCard,
+  IconButton
+} from '@contentful/forma-36-react-components';
+import { darken } from 'polished';
 
 const cardHeight = '100px';
 
@@ -10,6 +17,44 @@ const ModuleCardStyle = styled(EntryCard)`
   width: 100%;
 
   margin-bottom: 20px;
+`;
+
+const AddContentStyle = styled.div`
+  border: 1px dashed rgb(211, 220, 224);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  min-height: ${cardHeight};
+`;
+
+const colors = { default: '#2d64b3', hover: darken(0.2, '#2d64b3') };
+
+const AddContentButton = styled.span`
+  color: ${colors.default};
+  font-weight: bold;
+
+  &:hover {
+    color: ${colors.hover};
+  }
+
+  // icon button
+  svg {
+    fill: ${colors.default};
+    margin-right: 4px;
+
+    &:hover {
+      fill: ${colors.hover} !important; // override the default contentful color
+    }
+  }
+
+  cursor: pointer;
+  user-select: none;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const entriesById = {
@@ -40,28 +85,42 @@ function TemplateModules() {
     setEntries(toUpdate);
   };
 
-  return entries
-    .map((entryId) => entriesById[entryId])
-    .map(({ id, ...entryContent }) => (
-      <ModuleCardStyle
-        key={id}
-        cardDragHandleComponent={<CardDragHandle>Reorder card</CardDragHandle>}
-        dropdownListElements={
-          <>
-            <DropdownList>
-              <DropdownListItem isTitle>Actions</DropdownListItem>
-              <DropdownListItem onClick={() => removeItem(id)}>Remove</DropdownListItem>
-            </DropdownList>
-            <DropdownList>
-              <DropdownListItem onClick={() => moveToPosition(id, 0)}>Move to top</DropdownListItem>
-              <DropdownListItem onClick={() => moveToPosition(id, entries.length - 1)}>Move to bottom</DropdownListItem>
-            </DropdownList>
-          </>
-        }
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...entryContent}
-      />
-    ));
+  return (
+    <>
+      {entries
+        .map((entryId) => entriesById[entryId])
+        .map(({ id, ...entryContent }, index) => (
+          <ModuleCardStyle
+            key={id}
+            cardDragHandleComponent={<CardDragHandle>Reorder card</CardDragHandle>}
+            dropdownListElements={
+              <>
+                <DropdownList>
+                  <DropdownListItem isTitle>Actions</DropdownListItem>
+                  <DropdownListItem onClick={() => removeItem(id)}>Remove</DropdownListItem>
+                </DropdownList>
+                <DropdownList>
+                  <DropdownListItem onClick={() => moveToPosition(id, index - 1)}>Move up</DropdownListItem>
+                  <DropdownListItem onClick={() => moveToPosition(id, index + 1)}>Move down</DropdownListItem>
+                  <DropdownListItem onClick={() => moveToPosition(id, 0)}>Move to top</DropdownListItem>
+                  <DropdownListItem onClick={() => moveToPosition(id, entries.length - 1)}>
+                    Move to bottom
+                  </DropdownListItem>
+                </DropdownList>
+              </>
+            }
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...entryContent}
+          />
+        ))}
+      <AddContentStyle>
+        <AddContentButton>
+          <IconButton label="Add content" buttonType="primary" iconProps={{ icon: 'PlusCircle', size: 'small' }} />
+          <span>Add content</span>
+        </AddContentButton>
+      </AddContentStyle>
+    </>
+  );
 }
 
 export default TemplateModules;
