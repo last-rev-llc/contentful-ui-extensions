adaeba758bd6322f4b433b7f0a3e82b14311c0df
Will Whitty <tavurth@gmail.com>
2020-12-01 00:14:05 +0300

    Add select options

--- src/extensions/FormBuilder/FormBuilder.js
+++ src/extensions/FormBuilder/FormBuilder.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { curry, merge } from 'lodash/fp';
-import arrayMove from 'array-move';
+import React, { useEffect } from 'react';
+import { curry } from 'lodash/fp';
 
 import { useSDK } from '../../context';
 
@@ -12,85 +11,11 @@ import FieldModal from './StepList/FieldModal';
 
 import './FormBuilder.scss';
 import { buildStep } from './utils';
+import { useFormConfig, useFormSteps } from './hooks';
 
 function getModal(sdk) {
   const { modal } = sdk.parameters.invocation || {};
   return modal;
-}
-
-function useFormConfig(handleFieldChange, { title = '', type = 'custom' } = {}) {
-  const [values, setValues] = useState({ title, type });
-
-  return {
-    type: values.type,
-    title: values.title,
-    setType: (newType) => {
-      handleFieldChange('type', newType);
-      setValues((oldValues) => merge(oldValues)({ type: newType }));
-    },
-    setTitle: (newTitle) => {
-      handleFieldChange('title', newTitle);
-      setValues((oldValues) => merge(oldValues)({ title: newTitle }));
-    },
-    update: (updates) => setValues({ ...values, ...updates })
-  };
-}
-
-function useFormSteps(handleFieldChange, initialSteps = []) {
-  const [steps, setStepsBase] = useState(initialSteps);
-
-  const setSteps = (newValues) => {
-    if (newValues instanceof Function) {
-      return setStepsBase((oldValues) => {
-        const toReturn = newValues(oldValues);
-        handleFieldChange('steps', toReturn);
-        return toReturn;
-      });
-    }
-
-    handleFieldChange('steps', newValues);
-    return setStepsBase(newValues);
-  };
-
-  const stepAdd = () =>
-    setSteps((oldSteps) =>
-      // Generate a new empty step
-      oldSteps.concat(buildStep(`New Step ${steps.length + 1}`))
-    );
-
-  const stepRemove = ({ id: idToRemove }) =>
-    // Filter out old step by ID
-    setSteps((oldSteps) => oldSteps.filter(({ id }) => id !== idToRemove));
-
-  const stepEdit = (stepId, stepUpdates) =>
-    setSteps((oldSteps) =>
-      oldSteps.map((step) => {
-        // If matching ID found replace the step, else return old step
-        // We should pass the entire new step here to update
-        if (step.id !== stepId) return step;
-
-        // Allow the user to specify their own step updater function
-        if (stepUpdates instanceof Function) {
-          return stepUpdates(step);
-        }
-
-        // User has simply passed us an object to replace the step with
-        return stepUpdates;
-      })
-    );
-
-  const stepReorder = ({ oldIndex, newIndex }) =>
-    // Move the item to position requested
-    setSteps((oldSteps) => arrayMove(oldSteps, oldIndex, newIndex));
-
-  return {
-    steps,
-    stepAdd,
-    stepEdit,
-    stepRemove,
-    stepReorder,
-    update: setSteps
-  };
 }
 
 function FormBuilder() {
@@ -109,6 +34,11 @@ function FormBuilder() {
     buildStep('First step')
   ]);
 
+  /**
+   * Load the last saved state from contentful
+   * We'll fill out our independant configurations from this state
+   * (See above formConfig & stepConfig)
+   * */
   useEffect(
     () => {
       if (sdk.field?.getValue instanceof Function) {
@@ -144,7 +74,6 @@ function FormBuilder() {
       <FormInfo formConfig={formConfig} />
       <StepList stepConfig={stepConfig} />
       {/* <ConfirmDeleteDialog item={removeStep} onClose={handleCancelRemoveStep} onSubmit={handleRemoveStep} /> */}
-      {/* <SetupStep step={setupStep} onClose={handleCloseSetupStep} onSubmit={handleStepSubmit} /> */}
     </div>
   );
 }
--- src/extensions/FormBuilder/FormInfo/FormInfo.js
+++ src/extensions/FormBuilder/FormInfo/FormInfo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FieldGroup, FormLabel, TextInput, Select, Option } from '@contentful/forma-36-react-components';
+
+import { URL_TYPES } from '../utils';
 import SectionWrapper from '../SectionWrapper';
 
 const CreateForm = ({ formConfig }) => {
@@ -15,6 +17,8 @@ const CreateForm = ({ formConfig }) => {
           value={formConfig.title}
           onChange={(e) => formConfig.setTitle(e.currentTarget.value)}
         />
+      </FieldGroup>
+      <FieldGroup>
         <FormLabel htmlFor="type">Form Type</FormLabel>
         <Select
           required
@@ -26,15 +30,29 @@ const CreateForm = ({ formConfig }) => {
           <Option value="hubspot">Hubspot</Option>
         </Select>
       </FieldGroup>
+      {URL_TYPES.includes(formConfig.type) && (
+        <FieldGroup>
+          <FormLabel htmlFor="name">Form URL</FormLabel>
+          <TextInput
+            required
+            name="url"
+            id="url"
+            value={formConfig.url}
+            onChange={(e) => formConfig.setUrl(e.currentTarget.value)}
+          />
+        </FieldGroup>
+      )}
     </SectionWrapper>
   );
 };
 
 CreateForm.propTypes = {
   formConfig: PropTypes.shape({
+    url: PropTypes.string,
     type: PropTypes.string,
     title: PropTypes.string,
 
+    setUrl: PropTypes.func,
     setType: PropTypes.func,
     setTitle: PropTypes.func
   }).isRequired
