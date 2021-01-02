99029d9f1aa3cd07aacf7fca4bfe1d84fbb24194
Will Whitty <tavurth@gmail.com>
2021-01-02 14:25:09 +0300

    Style improvements

--- src/extensions/FormBuilder/StepList/FieldModal/FieldEditor.js
+++ src/extensions/FormBuilder/StepList/FieldModal/FieldEditor.js
@@ -1,8 +1,9 @@
 /* eslint-disable react/forbid-prop-types */
 
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { FieldGroup, FormLabel, TextInput } from '@contentful/forma-36-react-components';
+import { FieldGroup, TextField } from '@contentful/forma-36-react-components';
 import DependsOn from '../DependsOn';
 
 import SchemaEditor from './SchemaEditor';
@@ -11,21 +12,38 @@ import FieldTypeSelector from './FieldTypeSelector';
 import { errorOfType, errorTypes } from '../../validate';
 import { WarningStyle } from '../styles';
 
+const FieldEditorWrapper = styled.div`
+  > * {
+    margin-bottom: 1rem;
+  }
+`;
+
 function FieldEditor({ errors, field, updateField }) {
   const errorsForField = errors[field.id];
   const nameError = errorOfType(errorTypes.CONFLICT_NAME, errorsForField);
 
   return (
-    <>
+    <FieldEditorWrapper>
+      <TextField
+        required
+        labelText="Label"
+        defaultValue={field.label}
+        onChange={(e) => updateField('label', e.currentTarget.value)}
+      />
       <FieldGroup>
-        <FormLabel htmlFor="label">Label</FormLabel>
-        <TextInput required defaultValue={field.label} onChange={(e) => updateField('label', e.currentTarget.value)} />
-      </FieldGroup>
-      <FieldGroup>
-        <FormLabel htmlFor="title">Form key</FormLabel>
-        <TextInput required defaultValue={field.name} onChange={(e) => updateField('name', e.currentTarget.value)} />
+        <TextField
+          required
+          labelText="Form key"
+          defaultValue={field.name}
+          onChange={(e) => updateField('name', e.currentTarget.value)}
+        />
         {nameError && <WarningStyle>{nameError.message}</WarningStyle>}
       </FieldGroup>
+      <TextField
+        labelText="Placeholder"
+        defaultValue={field.placeholder}
+        onChange={(e) => updateField('placeholder', e.currentTarget.value)}
+      />
       <FieldTypeSelector field={field} updateField={updateField} errors={errors} />
       <SchemaEditor field={field} updateField={updateField} />
       <DependsOn
@@ -34,7 +52,7 @@ function FieldEditor({ errors, field, updateField }) {
         onChangeValue={updateField('dependsOn')}
         onChangeTests={updateField('dependsOnTests')}
       />
-    </>
+    </FieldEditorWrapper>
   );
 }
 
@@ -43,6 +61,7 @@ FieldEditor.propTypes = {
     id: PropTypes.string,
     label: PropTypes.string,
     name: PropTypes.string,
+    placeholder: PropTypes.string,
     dependsOn: PropTypes.string,
     dependsOnTests: PropTypes.arrayOf(PropTypes.string)
   }).isRequired,
