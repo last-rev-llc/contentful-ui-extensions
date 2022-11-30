85f9a3c9d6be384c5534bcf66a4e1c0275dec617
opassion <13392489+opassion@users.noreply.github.com>
2022-11-30 04:08:02 -0500

    Enable options editor for radio field type

--- src/extensions/FormBuilder/FieldModal/AdditionalFields/index.js
+++ src/extensions/FormBuilder/FieldModal/AdditionalFields/index.js
@@ -13,6 +13,7 @@ function AdditionalFields({ field, updateField, ...props }) {
     case 'hidden':
       return <Hidden key="hidden-field" field={field} updateField={updateField} {...props} />;
     case 'select':
+    case 'radio':
       return <Select key="select-field" field={field} updateField={updateField} {...props} />;
     case 'toggleable':
       return <Toggleable key="toggleable-field" field={field} updateField={updateField} {...props} />;
